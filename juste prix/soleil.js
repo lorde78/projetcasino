74657db4d3932471238fd406a86007ecf53963f5
Cherine Dexpert <cherine.dexpert@hetic.net>
2021-02-03 15:33:44 +0100

    test2

---
+++ juste prix/soleil.js
@@ -0,0 +1,14 @@
+
+let ChiffreAleatoire = Math.floor(Math.random()*10);
+console.log(ChiffreAleatoire) ;
+
+//var ChiffreRentre = input.innerHTML ; 
+//console.log(ChiffreRentre)
+
+var RecupChiffre = querySelector("input")
+
+
+if ( (ChiffreAleatoire) = RecupChiffre){
+    ChiffreRentre
+    } 
+
