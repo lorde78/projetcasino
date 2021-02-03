2fde84f774e2d671a9714876a4438d78f166c9b4
Cherine Dexpert <cherine.dexpert@hetic.net>
2021-02-03 16:53:21 +0100

    test

--- juste prix/soleil.js
+++ juste prix/soleil.js
@@ -1,14 +1,18 @@
 
-let ChiffreAleatoire = Math.floor(Math.random()*10);
-console.log(ChiffreAleatoire) ;
+//let ChiffreAleatoire = Math.floor(Math.random()*10);
+//console.log(ChiffreAleatoire) ;
 
 //var ChiffreRentre = input.innerHTML ; 
 //console.log(ChiffreRentre)
+var RecupChiffre = document.querySelector("input") ;
 
-var RecupChiffre = querySelector("input")
+function Valider() {
+    console.log(RecupChiffre)
+    alert(RecupChiffre)
+}
 
-
-if ( (ChiffreAleatoire) = RecupChiffre){
-    ChiffreRentre
-    } 
-
+//if (ChiffreAleatoire == RecupChiffre){
+    //console.log(RecupChiffre)
+    //} else {
+    //    alert("non")
+    //}
