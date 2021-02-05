289c75cc1f40eda1cd6b809bed31d534ae25d78f
lestox <78364535+lestox@users.noreply.github.com>
2021-02-05 22:19:46 +0100

    essai

--- juste prix/soleil.js
+++ juste prix/soleil.js
@@ -6,13 +6,20 @@ var clicks = 0;
 var score1 = 0;
 var score2 = 0;
 
+var compt_pop = 0;
+
+var node = document.getElementById("input");
+var pop_up = document.getElementsByClassName("pop_up");
+var plus = document.getElementById("plus");
+var moins = document.getElementById("moins");
+
 
 function ValiderChoix() {
     var RecupChiffre = document.getElementById("recup").value ;
     console.log(RecupChiffre);
     clicks += 1;
     document.getElementById("recup").innerHTML = clicks;
-    if (clicks == 4){
+    if (clicks == 10){
         if(RecupChiffre == ChiffreAleatoire) {
             alert("Cool");
             score1 = clicks
@@ -27,24 +34,36 @@ function ValiderChoix() {
             }
     } else {
         if (RecupChiffre > ChiffreAleatoire) {
-            document.getElementById("moins").style.display = "block";
-        } else {
-            if (RecupChiffre < ChiffreAleatoire){
-                document.getElementById("plus").style.display = "block";
-            }else {
-                if(RecupChiffre == ChiffreAleatoire) {
-                    alert("Cool");
-                    score1 = clicks
-                        document.getElementById("recup2").style.display = "block";
-                        document.getElementById("recup").style.display = "none";
-                        document.getElementById("button2").style.display = "block";
-                        document.getElementById("button").style.display = "none" ;
-                    
-                    
-                }
+            if (compt_pop == 0) {
+                moins.appendChild(node);
+                ("moins").style.display = "block";
+                compt_pop = 1;
             }
-        }           
+            
+            else {
+                node.insertBefore(moins)
+            }
+
+        } else if (RecupChiffre < ChiffreAleatoire){
+            if (compt_pop == 0) {
+                document.getElementById("plus").style.display = "block";
+                compt_pop = 1;
+            }
+            else {
+                //document.getElementById("plus").style.display = "block";
+            }
+
+        }else {
+            if(RecupChiffre == ChiffreAleatoire) {
+                alert("Cool");
+                score1 = clicks
+                    document.getElementById("recup2").style.display = "block";
+                    document.getElementById("recup").style.display = "none";
+                    document.getElementById("button2").style.display = "block";
+                    document.getElementById("button").style.display = "none" ;
+            // else    
+                
+            }
+        }
     }
-    
-    }
-    
+}
